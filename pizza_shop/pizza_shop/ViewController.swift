3236143c34a984881ad3582ba38da1b328e5cf23
Samet Ronaer <s.ronaer@hotmail.com>
2022-11-25 10:51:11 +0300

    Initial commit

---
+++ pizza_shop/pizza_shop/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  pizza_shop
+//
+//  Created by Samet Ronaer on 25.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
